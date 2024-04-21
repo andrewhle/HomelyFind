85037d828ef353e39bdff3384e7d008d1301a37a
andrewhle <andrewle0915@gmail.com>
2024-04-21 03:12:04 -0700

    make it better

--- components/SuggestionProperty.jsx
+++ components/SuggestionProperty.jsx
@@ -17,7 +17,7 @@ function SuggestionProperty() {
 
   return (
     <div className="flex flex-col items-center mb-10">
-      <div className="flex justify-between items-center w-full max-w-[1600px] h-full py-10 mt-5">
+      <div className="flex justify-between items-center w-full max-w-[900px] lg:max-w-[1024px] xl:max-w-[1280px] h-full py-10 mt-5">
         <h2 className="text-gray-800 text-4xl font-semibold font-sans">
           Homes For You
         </h2>
@@ -30,7 +30,7 @@ function SuggestionProperty() {
         <div className="pr-5">
             <FaArrowAltCircleLeft className="w-10 h-10 text-gray-800 hover:text-gray-600" onClick={slideLeft}/>
         </div>
-        <div id="slider" className="flex gap-5 max-w-[900px] lg:max-w-[1080px] xl:max-w-[1600px]  h-full overflow-x-scroll scroll scroll-smooth whitespace-nowrap">
+        <div id="slider" className="flex gap-5 max-w-[900px] lg:max-w-[1024px] xl:max-w-[1280px]  h-full overflow-x-scroll scroll scroll-smooth whitespace-nowrap">
           <Property image="property-1.jpeg" />
           <Property image="property-2.jpg" />
           <Property image="property-3.jpg" />
